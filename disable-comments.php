b866029d8ac0c10cee8d35f38347427a3a5e5b65
Jesse Nickles <jesse@littlebizzy.com>
2024-09-13 14:31:02 +0700

    Update disable-comments.php

--- disable-comments.php
+++ disable-comments.php
@@ -25,95 +25,110 @@ add_filter( 'gu_override_dot_org', function( $overrides ) {
 // Disable support for comments and trackbacks in post types
 function disable_comments_post_types_support() {
     $post_types = get_post_types();
-    foreach ($post_types as $post_type) {
-        if (post_type_supports($post_type, 'comments')) {
-            remove_post_type_support($post_type, 'comments');
-            remove_post_type_support($post_type, 'trackbacks');
+    
+    foreach ( $post_types as $post_type ) {
+        if ( post_type_supports( $post_type, 'comments' ) ) {
+            remove_post_type_support( $post_type, 'comments' );
+            remove_post_type_support( $post_type, 'trackbacks' );
         }
     }
 }
-add_action('admin_init', 'disable_comments_post_types_support');
+add_action( 'admin_init', 'disable_comments_post_types_support' );
 
 // Close comments on the front-end
 function disable_comments_status() {
     return false;
 }
-add_filter('comments_open', 'disable_comments_status', 20, 2);
-add_filter('pings_open', 'disable_comments_status', 20, 2);
+
+add_filter( 'comments_open', 'disable_comments_status', 20, 2 );
+add_filter( 'pings_open', 'disable_comments_status', 20, 2 );
 
 // Hide existing comments
-function disable_existing_comments($comments) {
+function disable_existing_comments( $comments ) {
     return array();
 }
-add_filter('comments_array', 'disable_existing_comments', 10, 2);
 
-// Remove comments page in menu
+add_filter( 'comments_array', 'disable_existing_comments', 20, 2 );
+
+// Remove comments page in menu for non-admin users
 function disable_comments_admin_menu() {
-    remove_menu_page('edit-comments.php');
+    if ( ! current_user_can( 'manage_options' ) ) {
+        remove_menu_page( 'edit-comments.php' );
+    }
 }
-add_action('admin_menu', 'disable_comments_admin_menu');
 
-// Redirect any user trying to access comments page
+add_action( 'admin_menu', 'disable_comments_admin_menu' );
+
+// Redirect non-admin users trying to access the comments page
 function disable_comments_admin_menu_redirect() {
     global $pagenow;
-    if ($pagenow === 'edit-comments.php') {
-        wp_redirect(admin_url());
+
+    if ( $pagenow === 'edit-comments.php' && ! current_user_can( 'manage_options' ) ) {
+        wp_safe_redirect( admin_url() );
         exit;
     }
 }
-add_action('admin_init', 'disable_comments_admin_menu_redirect');
+add_action( 'admin_init', 'disable_comments_admin_menu_redirect' );
 
-// Remove comments metabox from dashboard
+// Remove comments metabox from dashboard for non-admin users
 function disable_comments_dashboard() {
-    remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+    if ( ! current_user_can( 'manage_options' ) ) {
+        remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
+    }
 }
-add_action('admin_init', 'disable_comments_dashboard');
+add_action( 'admin_init', 'disable_comments_dashboard' );
 
 // Remove comments links from admin bar
 function disable_comments_admin_bar() {
-    if (is_admin_bar_showing()) {
-        remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
-    }
+    remove_action( 'admin_bar_menu', 'wp_admin_bar_comments_menu', 60 );
 }
-add_action('init', 'disable_comments_admin_bar');
+add_action( 'init', 'disable_comments_admin_bar' );
 
 // Remove X-Pingback header to prevent trackbacks
-add_filter('wp_headers', function($headers) {
-    unset($headers['X-Pingback']);
+function disable_pingback_header( $headers ) {
+    unset( $headers['X-Pingback'] );
     return $headers;
-});
+}
+add_filter( 'wp_headers', 'disable_pingback_header' );
 
 // Disable comment feed
 function disable_comment_feed() {
-    if (is_comment_feed()) {
-        wp_die(__('Comments are closed.', 'disable-comments'));
+    if ( is_comment_feed() ) {
+        wp_die( __( 'Comments feed is disabled on this site.', 'disable-comments' ), '', array( 'response' => 403 ) );
     }
 }
-add_action('template_redirect', 'disable_comment_feed');
+add_action( 'template_redirect', 'disable_comment_feed' );
 
 // Remove comment reply script
 function disable_comment_reply_script() {
-    wp_deregister_script('comment-reply');
+    wp_deregister_script( 'comment-reply' );
 }
-add_action('wp_enqueue_scripts', 'disable_comment_reply_script');
+add_action( 'wp_enqueue_scripts', 'disable_comment_reply_script' );
 
-// Remove discussion settings fields (from Simply Disable Comments)
+// Remove discussion settings fields
 function disable_discussion_settings_fields() {
-    add_filter('pre_option_default_ping_status', '__return_zero');
-    add_filter('pre_option_default_comment_status', '__return_zero');
-    add_action('admin_menu', function() {
-        remove_meta_box('commentstatusdiv', 'post', 'normal');
-        remove_meta_box('commentstatusdiv', 'page', 'normal');
-        remove_meta_box('trackbacksdiv', 'post', 'normal');
-    });
+    add_filter( 'pre_option_default_ping_status', '__return_zero' );
+    add_filter( 'pre_option_default_comment_status', '__return_zero' );
 }
-add_action('admin_init', 'disable_discussion_settings_fields');
 
-// Remove comment feed links from the header (from Simply Disable Comments)
-function disable_comment_feed_links() {
-    remove_action('wp_head', 'feed_links_extra', 3);
+// Remove meta boxes from all post types
+function disable_meta_boxes() {
+    $post_types = get_post_types();
+
+    foreach ( $post_types as $post_type ) {
+        remove_meta_box( 'commentstatusdiv', $post_type, 'normal' );
+        remove_meta_box( 'trackbacksdiv', $post_type, 'normal' );
+    }
 }
-add_action('wp_head', 'disable_comment_feed_links', 1);
+
+add_action( 'admin_init', 'disable_discussion_settings_fields' );
+add_action( 'admin_menu', 'disable_meta_boxes' );
+
+// Remove comment feed links from the header
+function disable_comment_feed_links() {
+    remove_action( 'wp_head', 'feed_links_extra', 3 );
+}
+add_action( 'wp_head', 'disable_comment_feed_links', 1 );
 
 // Ref: ChatGPT
 // Ref: https://github.com/HandyPlugins/simply-disable-comments/
